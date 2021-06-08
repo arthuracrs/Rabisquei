53bfbfe1104317d22538195ab92f7cf9a19e9b41
EC2 Default User <ec2-user@ip-172-31-34-74.sa-east-1.compute.internal>
2021-06-08 14:37:06 +0000

    move and centralize historic to the server

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const app = express();
 const http = require('http');
@@ -8,14 +7,77 @@ const socketIo = require("socket.io");
 const io = new socketIo.Server(server);
 
 app.use(express.static(__dirname + '/public'))
+// historic
+let historic = []
+let pointerHistoric = 0
+
+function decreasePointerHistoric() {
+    console.log('decrement chamado')
+    
+    if (pointerHistoric > 0) {
+        pointerHistoric--
+    }
+}
+
+function increasePointerHistoric() {
+    
+    console.log('increment chamado')
+    
+    if (pointerHistoric + 1 < historic.length) {
+        pointerHistoric++
+    }
+}
 
 io.on('connection', (socket) => {
     console.log('a user connected');
-    socket.on('draw', (obj) => {
+    
+    if(historic.length != 0 ){
+        io.emit('draw', historic[pointerHistoric])
+    }
+    
+    // socket.on('draw', (obj) => {
+    //     socket.broadcast.emit('draw', obj)
+    // })
+
+    socket.on('object:modified', obj => {
+
+        if (pointerHistoric + 1 < historic.length && historic.length != 1) {
+            historic = historic.slice(0, pointerHistoric + 1)
+        }
+
+        historic.push(obj)
+        increasePointerHistoric()
+        console.log('alteração')
+        console.log('tamanho do historico: '+historic.length)
+        console.log('posição: '+ pointerHistoric)
+        console.log()
         socket.broadcast.emit('draw', obj)
     })
-});
 
-server.listen(3030, () => {
-    console.log('listening on *:3000');
+    socket.on('undo', obj => {
+        decreasePointerHistoric()
+        
+        console.log('undo')
+        console.log('tamanho do historico: '+historic.length)
+        console.log('posição: '+ pointerHistoric)
+        console.log()
+        
+        io.emit('draw', historic[pointerHistoric])
+    })
+
+    socket.on('redo', obj => {
+        increasePointerHistoric()
+        
+        console.log('redo')
+        console.log('tamanho do historico: '+historic.length)
+        console.log('posição: '+ pointerHistoric)
+        console.log()
+        
+        io.emit('draw', historic[pointerHistoric])
+    })
+
+});
+const PORT = 3000
+server.listen(PORT, () => {
+    console.log(`listening on *:${PORT}`);
 });
--- public/index.js
+++ public/index.js
@@ -1,13 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     const socket = io.connect()
 
-    let kk = false
-
-    document.querySelector("#kk").addEventListener('click', () => {
-        kk = !kk
-        // console.log(kk)
-    })
-
     var canvas = new fabric.Canvas("c");
     var text = new fabric.Textbox('Hello world From Fabric JS', {
         width: 250,
@@ -19,72 +12,23 @@ document.addEventListener("DOMContentLoaded", () => {
     canvas.add(text)
 
     socket.on('draw', (obj) => {
-
-        // if (kk) {
-            canvas.loadFromJSON(obj)
-            console.log('sdas')
-        // }
-
+        canvas.loadFromJSON(obj)
     })
 
-    let historic = []
-    let pointerHistoric = 0
-
-    let tempCanvas = JSON.stringify(canvas)
-    historic.push(tempCanvas)
-
-    let saveCanvas = {}
-
-    function decreasePointerHistoric() {
-        if (pointerHistoric > 0) {
-            pointerHistoric--
-        }
-    }
-
-    function increasePointerHistoric() {
-        if (pointerHistoric + 1 < historic.length) {
-            pointerHistoric++
-        }
-    }
-
     canvas.on('after:render', (options) => {
-        document.querySelector('#pointer').innerHTML = 'Pointer: ' + pointerHistoric
-        document.querySelector('#length').innerHTML = 'Length: ' + historic.length
+
     })
 
     canvas.on('object:modified', (options) => {
-        tempCanvas = JSON.stringify(canvas)
-
-        if (pointerHistoric + 1 < historic.length && historic.length != 1) {
-            historic = historic.slice(0, pointerHistoric + 1)
-        }
-
-        historic.push(tempCanvas)
-        increasePointerHistoric()
-        
         const tempCanvasToEmit = JSON.stringify(canvas)
-        socket.emit('draw', tempCanvasToEmit)
-    })
-
-    document.querySelector("#save").addEventListener('click', () => {
-        tempCanvas = JSON.stringify(canvas)
-    })
-
-    document.querySelector("#load").addEventListener('click', () => {
-        canvas.loadFromJSON(tempCanvas)
+        socket.emit('object:modified', tempCanvasToEmit)
     })
 
     document.querySelector("#undo").addEventListener('click', () => {
-        decreasePointerHistoric()
-        canvas.loadFromJSON(historic[pointerHistoric])
+        socket.emit('undo')
     })
 
     document.querySelector("#redo").addEventListener('click', () => {
-        increasePointerHistoric()
-        canvas.loadFromJSON(historic[pointerHistoric])
+       socket.emit('redo')
     })
-
-    // console.log(JSON.stringify(canvas));//logs the string representation
-    // console.log(canvas.toObject());//logs canvas as an object
-    // console.log(canvas.toSVG());//logs the SVG representation of canvas
 })
